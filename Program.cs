b41357a3e048b4509d23e711600a48e45fe400d6
alexeizaigraev <neya1969@gmail.com>
2021-04-16 14:44:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpForPy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccBase.AccGetAll();
+        }
+
+        protected static void CheckOutText(string text)
+        {
+            if ("" == text) pRed("\nempty\n");
+            else pDarkYellow("\n\twell\n");
+        }
+
+        protected static void pRed(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+        }
+
+        protected static void pGreen(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+        }
+
+        protected static void pDarkYellow(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(text);
+        }
+        protected static void pYellow(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(text);
+        }
+
+        protected static void pDarkGray(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(text);
+        }
+        protected static void pGray(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(text);
+        }
+
+        protected static void pDarkBlue(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(text);
+        }
+        protected static void pBlue(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(text);
+        }
+
+        protected static void pDarkCyan(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(text);
+        }
+        protected static void pCyan(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(text);
+        }
+    }
+}
